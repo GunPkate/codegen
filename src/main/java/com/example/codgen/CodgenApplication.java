835d8d58522bdab260dae005d71af335c7453bff
GunPKate <gunPK>
2024-01-20 21:31:24 +0700

    initialize

---
+++ src/main/java/com/example/codgen/CodgenApplication.java
@@ -0,0 +1,13 @@
+package com.example.codgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodgenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codgen/CodgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
